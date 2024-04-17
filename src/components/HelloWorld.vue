6d68d073ecfd2115b7d78b294f53eb12c7177ff8
huanglj <huanglj@zenitera.com>
2024-04-17 13:59:46 +0800

    feature:测试提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { CSSProperties } from 'vue';
-const value = ref('horizontal');
-const baseStyle: CSSProperties = {
-  width: '25%',
-  height: '54px',
+import { reactive, ref } from 'vue';
+const justifyOptions = reactive([
+  'flex-start',
+  'center',
+  'flex-end',
+  'space-between',
+  'space-around',
+  'space-evenly',
+]);
+const alignOptions = reactive(['flex-start', 'center', 'flex-end']);
+const justify = ref(justifyOptions[0]);
+const alignItems = ref(alignOptions[0]);
+const boxStyle = {
+  width: '100%',
+  height: '120px',
+  borderRadius: '6px',
+  border: '1px solid #40a9ff',
 };
 </script>
 
 <template>
   <div>
-    <a-flex gap="middle" vertical>
-    <a-radio-group v-model:value="value">
-      <a-radio value="horizontal">horizontal</a-radio>
-      <a-radio value="vertical">vertical</a-radio>
-    </a-radio-group>
-    <a-flex :vertical="value === 'vertical'">
-      <div
-        v-for="(item, index) in new Array(4)"
-        :key="item"
-        :style="{ ...baseStyle, background: `${index % 2 ? '#1677ff' : '#1677ffbf'}` }"
-      />
+    <a-flex gap="middle" align="start" vertical>
+    <p>Select justify :</p>
+    <a-segmented v-model:value="justify" :options="justifyOptions" />
+    <p>Select align :</p>
+    <a-segmented v-model:value="alignItems" :options="alignOptions" />
+    <a-flex :style="{ ...boxStyle }" :justify="justify" :align="alignItems">
+      <a-button type="primary">Primary</a-button>
+      <a-button type="primary">Primary</a-button>
+      <a-button type="primary">Primary</a-button>
+      <a-button type="primary">Primary</a-button>
     </a-flex>
   </a-flex>
   </div>
